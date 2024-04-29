b536ce5f2d6acff2ecaee8996950a0aca2ce1146
moodiseo <moodiseo@gmail.com>
2024-04-29 17:56:13 +0900

    #1.5 Drawing Project Two

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,13 +3,16 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 250, 30);
+ctx.fillRect(200, 220, 25, 160);
+ctx.fillRect(400, 220, 25, 160);
+ctx.fillRect(258, 230, 110, 100);
+ctx.fillRect(270, 330, 25, 140);
+ctx.fillRect(330, 330, 25, 140);
 
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.arc(312, 190, 40, 0, 2 * Math.PI);
+ctx.fill();
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(295, 180, 7, Math.PI, 2 * Math.PI);
+ctx.arc(330, 180, 7, Math.PI, 2 * Math.PI);
 ctx.fill();
