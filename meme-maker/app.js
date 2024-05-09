9f4f8c79a601a9edde2bff6fed39915a6fa6c837
moodiseo <moodiseo@gmail.com>
2024-05-09 14:28:46 +0900

    #2.0 Painting Lines

--- meme-maker/app.js
+++ meme-maker/app.js
@@ -3,16 +3,25 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 220, 25, 160);
-ctx.fillRect(400, 220, 25, 160);
-ctx.fillRect(258, 230, 110, 100);
-ctx.fillRect(270, 330, 25, 140);
-ctx.fillRect(330, 330, 25, 140);
+ctx.lineWidth = 2;
 
-ctx.arc(312, 190, 40, 0, 2 * Math.PI);
-ctx.fill();
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(295, 180, 7, Math.PI, 2 * Math.PI);
-ctx.arc(330, 180, 7, Math.PI, 2 * Math.PI);
-ctx.fill();
+const colors = [
+  "#55efc4",
+  "#6c5ce7",
+  "#b2bec3",
+  "#d63031",
+  "#ffeaa7",
+  "#00b894",
+  "#e84393",
+  "#a29bfe",
+];
+function onClick(event) {
+  ctx.beginPath();
+  ctx.moveTo(400, 400);
+  const color = colors[Math.floor(Math.random() * colors.length)];
+  ctx.strokeStyle = color;
+  ctx.lineTo(event.offsetX, event.offsetY);
+  ctx.stroke();
+}
+
+canvas.addEventListener("mousemove", onClick);
